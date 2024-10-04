8e487a5bd289d9e220275d9d0e25175cf16f3f5f
Prajjwal998 <prajjwal.singhpratap@gmail.com>
2024-10-04 14:47:26 +0530

    resolve graph loading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role) # Permit role
+    params.require(:user).permit(:name,:email, :password, :password_confirmation, :role) # Permit role
   end
 
 end
